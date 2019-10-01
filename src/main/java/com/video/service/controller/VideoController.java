0350045c3c53d68654c1aa581960e81fb76b84e7
Banos Gábor <gabor9324@gmail.com>
2019-10-02 01:18:30 +0200

    add controller

---
+++ src/main/java/com/video/service/controller/VideoController.java
@@ -0,0 +1,20 @@
+package com.video.service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class VideoController {
+
+    @GetMapping("/")
+    public String getDemo(HttpServletRequest request) {
+
+        RestTemplate rest = new RestTemplate();
+        ResponseEntity<String> webPage = rest.getForEntity("https://ketfarkukutya.mkkp.party/", String.class);
+        return webPage.getBody();
+    }
+}
