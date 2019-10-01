8dec89692607076d28a39650fd38b6fa70b4d699
gabor <gabor9324@gmail.com>
2019-10-02 01:33:54 +0200

    Comment not used gradle elements

--- src/main/java/com/video/service/controller/VideoController.java
+++ src/main/java/com/video/service/controller/VideoController.java
@@ -1,6 +1,7 @@
 package com.video.service.controller;
 
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,10 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 public class VideoController {
 
     @GetMapping("/")
-    public String getDemo(HttpServletRequest request) {
-
+    public String getDemo() {
         RestTemplate rest = new RestTemplate();
-        ResponseEntity<String> webPage = rest.getForEntity("https://ketfarkukutya.mkkp.party/", String.class);
+        ResponseEntity<String> webPage = rest.getForEntity("https://ketfarkukutya.mkkp.party", String.class);
         return webPage.getBody();
     }
 }
